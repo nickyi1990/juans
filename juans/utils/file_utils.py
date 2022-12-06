e61a54f2a38517e607157229f50038fdb0a76379
鲲 <kun@B-YNCJQH2L-1928.local>
2022-12-06 23:48:46 +0800

    修改size表示单位

--- juans/utils/file_utils.py
+++ juans/utils/file_utils.py
@@ -2,11 +2,11 @@ import os
 
 
 def get_folder_size(folder_path="."):
-
+    """获取文件夹下所有文件的大小, 单位为MB"""
     size = 0
     Folderpath = folder_path
     for path, dirs, files in os.walk(Folderpath):
         for f in files:
             fp = os.path.join(path, f)
             size += os.path.getsize(fp)
-    print(f"Folder size: {int(size/1024/1024)} MB")
+    return round(size / 1024 / 1024, 2)
